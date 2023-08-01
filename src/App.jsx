b505d84a6157d81aae9fb1cd6883ac99b4566c5c
Denys Horkavyi <denisdenis2295@gmail.com>
2023-08-01 18:24:42 +0200

    feat: add logic with lights

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,58 @@ import './App.css'
 import { Counter } from './components/Counter'
 import { CounterInfo } from './components/CounterInfo'
 import { SingleLight } from './components/SingleLight'
+import { useState } from 'react'
+import { Grid } from './components/Grid'
 
 function App() {
+  const [rows, setRows] = useState(0)
+  const [columns, setColumns] = useState(0)
+
+  const handleDecrementColumn = () => {
+    if (columns <= 0) {
+      return
+    }
+    setColumns(columns - 1)
+  }
+  const handleIncrementColumn = () => {
+    setColumns(columns + 1)
+  }
+  const handleDecrementRow = () => {
+    if (rows <= 0) {
+      return
+    }
+    setRows(rows - 1)
+  }
+  const handleIncrementRow = () => {
+    setRows(rows + 1)
+  }
+  const totalLights = rows * columns
+
+  const lights = Array.from({ length: totalLights })
+
   return (
     <div>
+      Lights Generator
       <nav className={'nav'}>
-        <CounterInfo>0 bulbs</CounterInfo>
-        <Counter text={'columns'} count={0} />
-        <Counter text={'rows'} count={0} />
+        <CounterInfo>{totalLights}</CounterInfo>
+        <Counter
+          text={'columns'}
+          count={columns}
+          onIncrement={handleIncrementColumn}
+          onDecrement={handleDecrementColumn}
+        />
+        <Counter
+          text={'rows'}
+          count={rows}
+          onIncrement={handleIncrementRow}
+          onDecrement={handleDecrementRow}
+        />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, idx) => {
+          return <SingleLight key={idx} />
+        })}
+      </Grid>
     </div>
   )
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,29 +1,17 @@
-import { React, useState } from 'react'
 import { IconMinus } from './icons/IconMinus'
 import { IconPlus } from './icons/IconPlus'
 import { CounterInfo } from './CounterInfo'
 import styles from './Counter.module.css'
 
 export const Counter = (props) => {
-  const [value, setValue] = useState(0)
-
-  const handleDecrement = () => {
-    if (value <= 0) {
-      return
-    }
-    setValue(value - 1)
-  }
-  const handleIncrement = () => {
-    setValue(value + 1)
-  }
   return (
     <div className={styles.wrapper}>
       <CounterInfo>
-        {value} {props.text}
+        {props.count} {props.text}
       </CounterInfo>
       <nav>
-        <IconPlus onClick={handleIncrement} />
-        <IconMinus onClick={handleDecrement} />
+        <IconPlus onClick={props.onIncrement} />
+        <IconMinus onClick={props.onDecrement} />
       </nav>
     </div>
   )
