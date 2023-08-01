8ccb107139de9008769b34391dd33d49a096cbe7
Denys Horkavyi <denisdenis2295@gmail.com>
2023-08-01 18:37:12 +0200

    feat: randomize color light

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 import LightTurnedOff from '/images/light-turned-off.svg'
 import LightBlue from '/images/light-blue.svg'
+import LightGreen from '/public/images/light-green.svg'
+import LightOrange from '/public/images/light-orange.svg'
+import LightPink from '/public/images/light-pink.svg'
+import LightRed from '/public/images/light-red.svg'
+import LightYellow from '/public/images/light-yellow.svg'
+
+const lights = [
+  LightBlue,
+  LightGreen,
+  LightOrange,
+  LightPink,
+  LightRed,
+  LightYellow,
+]
 
 export const SingleLight = () => {
-  const [isOn, setIsOn] = useState(false)
+  const [isOn, setIsOn] = useState(true)
 
-  const imageSrc = isOn ? LightBlue : LightTurnedOff
+  const getRandomIndex = () => {
+    return Math.floor(Math.random() * lights.length)
+  }
+
+  const randomIndex = getRandomIndex()
+
+  const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff
 
   const handleLightToggle = () => {
     setIsOn(!isOn)
